precision error fix at anchor assignment

data_processing.py
@@ -384,8 +384,15 @@ class DataManager:
 
             return intersection / union
 
+        __idx = 0
+
         for purpose in ["train", "validation"]:
             for img_id in self.imgs[purpose].keys():
+
+                if __idx > 8:
+                    break
+
+                __idx += 1
 
                 bool_mask = []
                 target_mask = []
@@ -395,8 +402,8 @@ class DataManager:
                     #bool_mask.append([[[[0] for _ in range(ANCHOR_PERSCALE_CNT)] for _ in range(GRID_CELL_CNT[d])] for _ in range(GRID_CELL_CNT[d])])
                     #target_mask.append([[[[0 for _ in range(4 + len(self.category_onehot_to_id))] for _ in range(ANCHOR_PERSCALE_CNT)] for _ in range(GRID_CELL_CNT[d])] for _ in range(GRID_CELL_CNT[d])])
 
-                    bool_mask.append(np.array([[[[0] for _ in range(ANCHOR_PERSCALE_CNT)] for _ in range(GRID_CELL_CNT[d])] for _ in range(GRID_CELL_CNT[d])]))
-                    target_mask.append(np.array([[[[0 for _ in range(4 + len(self.category_onehot_to_id))] for _ in range(ANCHOR_PERSCALE_CNT)] for _ in range(GRID_CELL_CNT[d])] for _ in range(GRID_CELL_CNT[d])]))
+                    bool_mask.append(np.array([[[[0] for _ in range(ANCHOR_PERSCALE_CNT)] for _ in range(GRID_CELL_CNT[d])] for _ in range(GRID_CELL_CNT[d])], dtype=np.float64))
+                    target_mask.append(np.array([[[[0 for _ in range(4 + len(self.category_onehot_to_id))] for _ in range(ANCHOR_PERSCALE_CNT)] for _ in range(GRID_CELL_CNT[d])] for _ in range(GRID_CELL_CNT[d])], dtype=np.float64))
 
                 for bbox_d in self.imgs[purpose][img_id]["objs"]:
 
@@ -434,19 +441,13 @@ class DataManager:
                     anchor_w = GRID_CELL_CNT[max_iou_scale] * (self.anchors[max_iou_scale][max_iou_idx][0] / IMG_SIZE[0])
                     anchor_h = GRID_CELL_CNT[max_iou_scale] * (self.anchors[max_iou_scale][max_iou_idx][1] / IMG_SIZE[0])
 
-                    bool_mask[max_iou_scale][cx][cy][max_iou_idx] = np.array([1])
-                    '''target_mask[max_iou_scale][cx][cy][max_iou_idx] = tf.concat([tf.convert_to_tensor([tf.math.log(x / (1 - x))]), 
-                                                                                tf.convert_to_tensor([tf.math.log(y / (1 - y))]),
-                                                                                tf.convert_to_tensor([tf.math.log(w / anchor_w)]), 
-                                                                                tf.convert_to_tensor([tf.math.log(h / anchor_h)]),
-                                                                                tf.cast(tf.one_hot(categ, len(self.category_onehot_to_id)), dtype=tf.double)],
-                                                                                axis=0)'''
-                    target_mask[max_iou_scale][cx][cy][max_iou_idx] = np.concatenate([np.array([tf.math.log(x / (1 - x))]), 
-                                                                                        np.array([tf.math.log(y / (1 - y))]),
-                                                                                        np.array([tf.math.log(w / anchor_w)]), 
-                                                                                        np.array([tf.math.log(h / anchor_h)]),
-                                                                                        np.array(tf.one_hot(categ, len(self.category_onehot_to_id)))],
-                                                                                        axis=0)
+                    bool_mask[max_iou_scale][cx][cy][max_iou_idx] = np.array([1.0], dtype=np.float64)
+                    target_mask[max_iou_scale][cx][cy][max_iou_idx] = np.array(tf.concat([tf.convert_to_tensor([tf.math.log(x / (1 - x))]), 
+                                                                                            tf.convert_to_tensor([tf.math.log(y / (1 - y))]),
+                                                                                            tf.convert_to_tensor([tf.math.log(w / anchor_w)]), 
+                                                                                            tf.convert_to_tensor([tf.math.log(h / anchor_h)]),
+                                                                                            tf.cast(tf.one_hot(categ, len(self.category_onehot_to_id)), dtype=tf.double)],
+                                                                                            axis=0), dtype=np.float64)
 
                 for d in range(SCALE_CNT):
 

model.py
@@ -9,9 +9,6 @@ from loss import *
 from predictions import *
 from data_processing import *
 
-print("FIXME: RE-INTRODUCE BATCH NORM WHEN FINISHING TESTS WITH 1 IMAGE")
-print("TODO: ADD MODEL LOAD AND SAVE")
-
 class ConvLayer(tf.keras.layers.Layer):
 
     LEAKY_RELU_RATE = 0.1
@@ -66,6 +63,8 @@ class ResSequence(tf.keras.layers.Layer):
         return y
 
 class Network:
+
+    MODELS_PATH = "./saved_models"
 
     def __init__(self, data_manager):
 
@@ -454,3 +453,21 @@ class Network:
             output_class_maxp = [output_class_maxp_scale0, output_class_maxp_scale1, output_class_maxp_scale2]
 
             show_prediction(np.array(img[0]), output_xy_min, output_xy_max, output_class, output_class_maxp, self.data_manager.onehot_to_name)
+
+    # FIXME add optimizers
+    def load_model(self, name):
+        
+        print("Loading model...")
+        self.full_network = tf.keras.models.load_model(f"{Network.MODELS_PATH}/{name}", custom_objects={
+                                                                                                        "ConvLayer": ConvLayer,
+                                                                                                        "ResBlock": ResBlock, 
+                                                                                                        "ResSequence": ResSequence
+                                                                                                        }
+                                                        )
+
+
+    # FIXME add optimizers
+    def save_model(self, name):
+
+        print("Saving model...")
+        tf.keras.models.save_model(self.full_network, f"{Network.MODELS_PATH}/{name}", overwrite=False)